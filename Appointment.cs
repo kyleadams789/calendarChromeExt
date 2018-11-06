8f2c098fb16b67ec608de72fff5766dccc38e18c
Taylor-Grover <taylor.j.grover@gmail.com>
2018-11-06 10:33:52 -0700

    finished upload

---
+++ Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Appointment
+{
+    public Appointment() {  }
+
+        public string emailAddress;
+        public string Date;
+        public string apptName;
+
+	public void setEmail(string email)
+{
+   emailAdress = email;
+}
+    public void setDate(string newDate)
+    {
+        Date = newDate;
+    }
+    public void setAppt(string appt)
+    {
+        apptName = appt;
+    }
+
+
+}
